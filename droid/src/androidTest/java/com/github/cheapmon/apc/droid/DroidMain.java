e4adc7324db13256e11525186a8c68f5b4bcf3f3
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2018-01-29 15:06:27 +0100

    Parse options in submodule

--- droid/src/androidTest/java/com/github/cheapmon/apc/droid/DroidMain.java
+++ droid/src/androidTest/java/com/github/cheapmon/apc/droid/DroidMain.java
@@ -1,5 +1,7 @@
 package com.github.cheapmon.apc.droid;
 
+import android.os.Bundle;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 import com.github.cheapmon.apc.droid.util.DroidLogger;
 import org.junit.Test;
@@ -19,11 +21,45 @@ import org.junit.runner.RunWith;
 public class DroidMain {
 
   /**
+   * Labels of Android apps
+   */
+  private String[] ids;
+
+  /**
+   * Extraction mode to use
+   */
+  private String mode;
+
+  /**
+   * Name of search algorithm to use in extraction
+   */
+  private String algorithm;
+
+  /**
    * Entry point for Droid pipeline. Check for arguments. Configure extraction.
    */
   @Test
   public void main() {
+    parseCommands();
+  }
+
+  /**
+   * Parse arguments given to Droid.
+   */
+  private void parseCommands() {
+    Bundle extras = InstrumentationRegistry.getArguments();
+    this.ids = extras.getString("ids", "").split(",");
+    this.mode = extras.getString("mode");
+    this.algorithm = extras.getString("algorithm");
     DroidLogger.log("Droid");
+    DroidLogger.space();
+    DroidLogger.log("Found ids:");
+    for (String id : this.ids) {
+      DroidLogger.log(String.format("* %s", id));
+    }
+    DroidLogger.log(String.format("Extraction mode is %s", this.mode));
+    DroidLogger.log(String.format("Using %s", this.algorithm));
+    DroidLogger.space();
   }
 
 }
