42d3d57936559c5671c13a4845af2cde1da3b960
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2018-01-31 16:07:45 +0100

    Improve installing and removing apps
    * Use container variables
    * Handle edge cases
    * Wait for window update

--- droid/src/androidTest/java/com/github/cheapmon/apc/droid/PlaystoreInstaller.java
+++ droid/src/androidTest/java/com/github/cheapmon/apc/droid/PlaystoreInstaller.java
@@ -10,6 +10,11 @@ import android.support.test.uiautomator.By;
 import android.support.test.uiautomator.BySelector;
 import android.support.test.uiautomator.UiDevice;
 import android.support.test.uiautomator.Until;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Install or remove Android application from Google Play.
@@ -26,32 +31,85 @@ public class PlaystoreInstaller {
   }
 
   /**
+   * Default timeout for actions on device.
+   */
+  private static final int TIMEOUT = 1000;
+
+  /**
    * Install application.<br><br>
    *
-   * Very basic approach. Click install buttons and give permissions. Fail on warning message.
+   * <ul>
+   * <li>Find install buttons and click</li>
+   * <li>Give permissions</li>
+   * <li>Fail on warning message</li>
+   * <li>Skip account completion</li>
+   * <li>Allow big downloads</li>
+   * <li>Wait for download and installation to finish</li>
+   * </ul>
    *
    * @param id App id
    * @throws RemoteException Device communication fails
    */
   public static InstallState install(String id) throws RemoteException {
+    BySelector warningMessage = By.res("com.android.vending:id/warning_message_module");
+    BySelector wifiMessage = By.res("com.android.vending:id/wifi_message");
+    BySelector installMessage = By.res("com.android.vending:id/summary_dynamic_status")
+        .clazz("android.widget.TextView");
+    BySelector message = By.res("android:id/message");
+    BySelector buttonContainer = By.res("com.android.vending:id/button_container");
+    BySelector buttonPanel = By.res("com.android.vending:id/buttonPanel");
+    BySelector downloadPanel = By.res("com.android.vending:id/download_progress_panel");
+    BySelector appPermissions = By.res("com.android.vending:id/app_permissions");
+    BySelector continueBar = By.res("com.android.vending:id/continue_button_bar");
+    BySelector continueButton = By.res("com.android.vending:id/continue_button");
+    BySelector skipButton = By.res("com.android.vending:id/not_now_button");
+    BySelector button = By.clazz("android.widget.Button");
+    BySelector firstButton = By.res("android:id/button1");
+    UiDevice device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
     if (installed(id)) {
       return InstallState.ALREADY_INSTALLED;
     }
     openPlaystore(id);
-    UiDevice device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
-    if (device.hasObject(By.res("com.android.vending:id/warning_message_module"))) {
+    device.waitForWindowUpdate("com.android.vending", TIMEOUT);
+    if (device.hasObject(warningMessage)) {
       return InstallState.FAILURE;
     }
-    BySelector buttonContainerSelector = By.res("com.android.vending:id/button_container");
-    device.findObject(buttonContainerSelector).findObject(By.clazz("android.widget.Button"))
-        .click();
-    device.wait(Until.gone(buttonContainerSelector), 5000);
-    if (device.hasObject(By.res("com.android.vending:id/app_permissions"))) {
-      device.findObject(By.res("com.android.vending:id/continue_button_bar"))
-          .findObject(By.res("com.android.vending:id/continue_button"))
-          .click();
+    device.wait(Until.hasObject(buttonContainer), TIMEOUT);
+    device.findObject(buttonContainer).findObject(button).click();
+    device.waitForWindowUpdate("com.android.vending", TIMEOUT);
+    device.wait(Until.gone(buttonContainer), TIMEOUT);
+    if (device.hasObject(wifiMessage)) {
+      device.findObject(buttonPanel).findObject(firstButton).click();
+      device.waitForWindowUpdate("com.android.vending", TIMEOUT);
+      device.wait(Until.gone(wifiMessage), TIMEOUT);
     }
-    device.wait(Until.hasObject(buttonContainerSelector), 5000);
+    if (device.hasObject(buttonPanel)) {
+      device.findObject(buttonPanel).findObject(button).click();
+      device.waitForWindowUpdate("com.android.vending", TIMEOUT);
+      device.wait(Until.hasObject(skipButton), TIMEOUT);
+      device.findObject(skipButton).click();
+      device.waitForWindowUpdate("com.android.vending", TIMEOUT);
+      device.wait(Until.gone(buttonPanel), TIMEOUT);
+    }
+    if (device.hasObject(appPermissions)) {
+      device.findObject(continueBar).findObject(continueButton).click();
+      device.waitForWindowUpdate("com.android.vending", TIMEOUT);
+      device.wait(Until.gone(appPermissions), TIMEOUT);
+    }
+    device.wait(Until.hasObject(downloadPanel), TIMEOUT);
+    while (device.hasObject(downloadPanel)) {
+      device.wait(Until.gone(downloadPanel), TIMEOUT);
+    }
+    while (device.hasObject(installMessage)) {
+      device.wait(Until.gone(installMessage), TIMEOUT);
+    }
+    if (device.hasObject(message)) {
+      device.findObject(buttonPanel).findObject(firstButton).click();
+      device.waitForWindowUpdate("com.android.vending", TIMEOUT);
+      device.wait(Until.gone(message), TIMEOUT);
+      return InstallState.FAILURE;
+    }
+    device.wait(Until.hasObject(buttonContainer), TIMEOUT);
     return InstallState.SUCCESS;
   }
 
@@ -64,16 +122,26 @@ public class PlaystoreInstaller {
    * @throws RemoteException Device communication fails
    */
   public static void remove(String id) throws RemoteException {
-    openPlaystore(id);
+    BySelector message = By.res("android:id/message");
+    BySelector buttonContainer = By.res("com.android.vending:id/button_container");
+    BySelector buttonPanel = By.res("com.android.vending:id/buttonPanel");
+    BySelector button = By.clazz("android.widget.Button");
+    BySelector firstButton = By.res("android:id/button1");
     UiDevice device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
-    BySelector buttonContainerSelector = By.res("com.android.vending:id/button_container");
-    BySelector buttonPanelSelector = By.res("com.android.vending:id/buttonPanel");
-    device.findObject(buttonContainerSelector)
-        .findObject(By.clazz("android.widget.Button")).click();
-    device.wait(Until.hasObject(buttonPanelSelector), 5000);
-    device.findObject(buttonPanelSelector).findObject(By.res("android:id/button1")).click();
-    device.wait(Until.gone(buttonPanelSelector), 5000);
-    device.wait(Until.hasObject(buttonContainerSelector), 5000);
+    openPlaystore(id);
+    device.waitForWindowUpdate("com.android.vending", TIMEOUT);
+    device.wait(Until.hasObject(buttonContainer), TIMEOUT);
+    device.findObject(buttonContainer).findObject(button).click();
+    device.waitForWindowUpdate("com.android.vending", TIMEOUT);
+    device.wait(Until.hasObject(buttonPanel), TIMEOUT);
+    device.findObject(buttonPanel).findObject(firstButton).click();
+    device.waitForWindowUpdate("com.android.vending", TIMEOUT);
+    device.wait(Until.gone(buttonPanel), TIMEOUT);
+    device.wait(Until.hasObject(message), TIMEOUT);
+    while (device.hasObject(message)) {
+      device.wait(Until.gone(message), TIMEOUT);
+    }
+    device.wait(Until.hasObject(buttonContainer), TIMEOUT);
   }
 
   /**
