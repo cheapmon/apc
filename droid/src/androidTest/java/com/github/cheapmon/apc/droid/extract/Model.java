044125b6f557093503d702dbb39ccccd6a373c14
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2018-02-26 11:59:34 +0100

    Model and extraction shell

---
+++ droid/src/androidTest/java/com/github/cheapmon/apc/droid/extract/Model.java
@@ -0,0 +1,20 @@
+package com.github.cheapmon.apc.droid.extract;
+
+/**
+ * Model of an app.<br><br>
+ *
+ * This model is tree-like. Every node corresponds to an activity and its possible layouts and links
+ * to other activities reachable by clicking. Every layout is described in its text contents, object
+ * hierarchy etc.
+ *
+ * @author <a href="mailto:simon.kaleschke.leipzig@gmail.com">cheapmon</a>
+ */
+public class Model {
+
+  /**
+   * Instantiate new model.
+   */
+  public Model() {
+  }
+
+}
---
+++ droid/src/androidTest/java/com/github/cheapmon/apc/droid/extract/ModelExtractor.java
@@ -0,0 +1,26 @@
+package com.github.cheapmon.apc.droid.extract;
+
+/**
+ * Extract a model of an app.<br><br>
+ *
+ * <ul>
+ * <li>Go through app</li>
+ * <li>Collect internal representation of layout</li>
+ * <li>Stop at certain threshold</li>
+ * </ul>
+ *
+ * @author <a href="mailto:simon.kaleschke.leipzig@gmail.com">cheapmon</a>
+ */
+public class ModelExtractor {
+
+  /**
+   * Get model of an app.
+   *
+   * @param id App identification
+   * @return Model of app
+   */
+  public static Model get(String id) {
+    return null;
+  }
+
+}
