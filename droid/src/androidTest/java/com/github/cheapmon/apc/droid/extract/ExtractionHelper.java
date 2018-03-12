0f8abf47f393257b6dd74c7af503161db618fc3e
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2018-03-12 11:56:26 +0100

    Bind single application to helper

--- droid/src/androidTest/java/com/github/cheapmon/apc/droid/extract/ExtractionHelper.java
+++ droid/src/androidTest/java/com/github/cheapmon/apc/droid/extract/ExtractionHelper.java
@@ -1,6 +1,11 @@
 package com.github.cheapmon.apc.droid.extract;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.uiautomator.By;
 import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.Until;
 import com.github.cheapmon.apc.droid.util.DroidException;
 import java.io.IOException;
 
@@ -12,14 +17,60 @@ import java.io.IOException;
 public class ExtractionHelper {
 
   /**
-   * Get current activity on running device.
+   * ID of application this helper belongs to
+   */
+  private final String applicationID;
+
+  /**
+   * Device this helper belongs to
+   */
+  private final UiDevice device;
+
+  /**
+   * Launcher context for application
+   */
+  private final Context launchContext;
+
+  /**
+   * Launcher intent for application
+   */
+  private final Intent launchIntent;
+
+  /**
+   * Timeout for application loading
+   */
+  private final int TIMEOUT = 5000;
+
+  /**
+   * Get new helper for certain application
    *
-   * @param device Device to search
+   * @param applicationID ID of application
+   */
+  public ExtractionHelper(String applicationID) {
+    this.applicationID = applicationID;
+    this.device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+    this.launchContext = InstrumentationRegistry.getContext();
+    this.launchIntent = launchContext.getPackageManager()
+        .getLaunchIntentForPackage(this.applicationID);
+    this.launchIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+  }
+
+  /**
+   * Start activity from scratch.
+   */
+  public void start() {
+    launchContext.startActivity(launchIntent);
+    device.wait(Until.hasObject(By.pkg(this.applicationID).depth(0)), TIMEOUT);
+  }
+
+  /**
+   * Get current activity.
+   *
    * @return Activity name
    */
-  public static String getActivityName(UiDevice device) throws DroidException {
+  public String getActivityName() throws DroidException {
     try {
-      String out = device.executeShellCommand("dumpsys window windows");
+      String out = this.device.executeShellCommand("dumpsys window windows");
       for (String line : out.split("\n")) {
         if (line.contains("mFocusedApp=")) {
           for (String part : line.split(" ")) {
